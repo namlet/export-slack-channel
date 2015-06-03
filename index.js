f3039e41e1ba26e41c4a0da759295956ee2a94f1
Nathan Loyer <4namlet@gmail.com>
2015-06-03 14:17:42 -0500

    parse results

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
-app.get('/channel', function(req, res, next){
+app.get('/channel/:channel', function(req, res, next){
   var api_key = process.env.IH_API_KEY;
 
   request({
-    uri: 'https://slack.com/api/channels.list',
+    uri: 'https://slack.com/api/channels.history',
     qs: {
-      token: api_key
+      token: api_key,
+      channel: req.params.channel,
+      count: 100,
+      pretty: 1
     }
   }, function(err, response, body){
-    console.log(response);
+    var payload = JSON.parse(body);
+
+    var result = payload.messages.map(function(obj){
+      return obj.text;
+    });
+    res.send(result.join('<br><br>'));
   });
 
 });
