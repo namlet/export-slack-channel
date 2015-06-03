14f125a1e7c91298a58686567f04abe2621cb197
Nathan Loyer <4namlet@gmail.com>
2015-06-03 14:56:22 -0500

    parse channel text to replace channel id with link to that channel with readable name

--- index.js
+++ index.js
@@ -4,22 +4,39 @@ var request = require('request');
 
 app.get('/channel/:channel', function(req, res, next){
   var api_key = process.env.IH_API_KEY;
+  var channels = {};
 
   request({
-    uri: 'https://slack.com/api/channels.history',
+    uri: 'https://slack.com/api/channels.list',
     qs: {
       token: api_key,
-      channel: req.params.channel,
-      count: 100,
       pretty: 1
     }
   }, function(err, response, body){
     var payload = JSON.parse(body);
 
-    var result = payload.messages.map(function(obj){
-      return obj.text;
+    payload.channels.map(function(obj){
+      channels[obj.id] = obj.name;
     });
-    res.send(result.join('<br><br>'));
+
+    request({
+      uri: 'https://slack.com/api/channels.history',
+      qs: {
+        token: api_key,
+        channel: req.params.channel,
+        count: 200,
+        pretty: 1
+      }
+    }, function(err, response, body){
+      var payload = JSON.parse(body);
+
+      var result = payload.messages.map(function(obj){
+        var parsed_text = obj.text.replace(/<#([^>]+)>/g, function(m,sub1){ return "<a href='https://invisionheart.slack.com/messages/"+channels[sub1]+"/'>#"+channels[sub1]+"</a>"; });
+        return parsed_text;
+      });
+      res.send(result.join('<br><br>'));
+    });
+
   });
 
 });
